48dd5c48438bbe81d99052aa3fd551afc048662b
User <user@mail.net>
2024-01-23 21:59:50 +0200

    Initial Commit

---
+++ HWS_BetterRest_learning/HWS_BetterRest_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_BetterRest_learningApp.swift
+//  HWS_BetterRest_learning
+//
+//  Created by Server Admin on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HWS_BetterRest_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
