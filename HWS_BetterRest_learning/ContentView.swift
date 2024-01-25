fd4d2d259645afcd686e663636fb6c58b10a4bd4
User <user@mail.net>
2024-01-25 19:05:55 +0200

    Selecting dates and times with DatePicker	 	learning

--- HWS_BetterRest_learning/ContentView.swift
+++ HWS_BetterRest_learning/ContentView.swift
@@ -10,13 +10,33 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepHours = 8.0
+    @State private var wakeUp = Date.now
+    
     
     var body: some View {
         
         Stepper("\(sleepHours.formatted()) hours", value: $sleepHours, in: 4...12, step: 0.5)
             .padding()
+        DatePicker("Pleas take data and time wake up", selection: $wakeUp, in: ...Date.now, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+            
         
     }
+    
+    
+    func exampleDates() {
+        // create a second Date instance set to one day in seconds from now
+        let tomorrow = Date.now.addingTimeInterval(86400)
+
+        // create a range from those two
+        let range = Date.now...tomorrow
+    }
+    
+    
+    
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
