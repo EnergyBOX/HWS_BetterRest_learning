54813bf8ebd03fb8db0d866aacb0308f6513aeee
User <user@mail.net>
2024-01-26 22:09:17 +0200

    Connecting SwiftUI to Core ML  learning

--- HWS_BetterRest_learning/ContentView.swift
+++ HWS_BetterRest_learning/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Server Admin on 23.01.2024.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -13,7 +14,9 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         
@@ -35,8 +38,33 @@ struct ContentView: View {
                 Button ("Calculate", action: calculateBedTime)
             }
         }
+        .alert(alertTitle, isPresented: $showingAlert) {
+            Button("OK") { }
+        } message: {
+            Text(alertMessage)
+        }
     }
     func calculateBedTime(){
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
         
     }
     
